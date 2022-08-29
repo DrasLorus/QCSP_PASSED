2b43d67011e534b05d32edeace77bba28a8cdc1c
Camille Moniere <camille.moniere@univ-ubs.fr>
2022-08-29 22:06:04 +0200

    Will clearly use configure approach

--- sources/simulators/fa_md_scores/main.cpp
+++ sources/simulators/fa_md_scores/main.cpp
@@ -26,123 +26,14 @@ enum sim_type_t {
     MD
 };
 
-constexpr int defines_line                    = __LINE__;
-constexpr int p_omega_count                   = 9;
-constexpr int p_omega_accepted[p_omega_count] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-constexpr int q_count                         = 4;
-constexpr int q_accepted[q_count]             = {64, 128, 256, 512};
-constexpr int N_count                         = 1;
-constexpr int N_accepted[1]                   = {60};
-
-#define switch_create_p_omega(ptr, CNidx, CQidx, Oidx)                                                      \
-    switch (Oidx) {                                                                                         \
-        case 0:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[0]>>(); \
-            break;                                                                                          \
-        case 1:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[1]>>(); \
-            break;                                                                                          \
-        case 2:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[2]>>(); \
-            break;                                                                                          \
-        case 3:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[3]>>(); \
-            break;                                                                                          \
-        case 4:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[4]>>(); \
-            break;                                                                                          \
-        case 5:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[5]>>(); \
-            break;                                                                                          \
-        case 6:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[6]>>(); \
-            break;                                                                                          \
-        case 7:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[7]>>(); \
-            break;                                                                                          \
-        case 8:                                                                                             \
-            ptr = new vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[8]>>(); \
-            break;                                                                                          \
-    }
-
-#define switch_create_q(ptr, CNidx, Qidx, Oidx)         \
-    switch (Qidx) {                                     \
-        case 0:                                         \
-            switch_create_p_omega(ptr, CNidx, 0, Oidx); \
-            break;                                      \
-        case 1:                                         \
-            switch_create_p_omega(ptr, CNidx, 1, Oidx); \
-            break;                                      \
-        case 2:                                         \
-            switch_create_p_omega(ptr, CNidx, 2, Oidx); \
-            break;                                      \
-        case 3:                                         \
-            switch_create_p_omega(ptr, CNidx, 3, Oidx); \
-            break;                                      \
-    }
-
-#define allocate_detector_vector(ptr, Nidx, Qidx, Oidx) \
-    switch (Nidx) {                                     \
-        case 0:                                         \
-            switch_create_q(ptr, 0, Qidx, Oidx);        \
-            break;                                      \
-    }
-
-#define switch_delete_p_omega(ptr, CNidx, CQidx, Oidx)                                                         \
-    switch (Oidx) {                                                                                            \
-        case 0:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[0]>> *) ptr; \
-            break;                                                                                             \
-        case 1:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[1]>> *) ptr; \
-            break;                                                                                             \
-        case 2:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[2]>> *) ptr; \
-            break;                                                                                             \
-        case 3:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[3]>> *) ptr; \
-            break;                                                                                             \
-        case 4:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[4]>> *) ptr; \
-            break;                                                                                             \
-        case 5:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[5]>> *) ptr; \
-            break;                                                                                             \
-        case 6:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[6]>> *) ptr; \
-            break;                                                                                             \
-        case 7:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[7]>> *) ptr; \
-            break;                                                                                             \
-        case 8:                                                                                                \
-            delete (vector<CDetectorSerial<N_accepted[CNidx], q_accepted[CQidx], p_omega_accepted[8]>> *) ptr; \
-            break;                                                                                             \
-    }
-
-#define switch_delete_q(ptr, CNidx, Qidx, Oidx)         \
-    switch (Qidx) {                                     \
-        case 0:                                         \
-            switch_delete_p_omega(ptr, CNidx, 0, Oidx); \
-            break;                                      \
-        case 1:                                         \
-            switch_delete_p_omega(ptr, CNidx, 1, Oidx); \
-            break;                                      \
-        case 2:                                         \
-            switch_delete_p_omega(ptr, CNidx, 2, Oidx); \
-            break;                                      \
-        case 3:                                         \
-            switch_delete_p_omega(ptr, CNidx, 3, Oidx); \
-            break;                                      \
-    }
-
-#define delete_detector_vector(ptr, Nidx, Qidx, Oidx) \
-    switch (Nidx) {                                   \
-        case 0:                                       \
-            switch_delete_q(ptr, 0, Qidx, Oidx);      \
-            break;                                    \
-    }
+static constexpr int cst_q       = 64;
+static constexpr int cst_N       = 60;
+static constexpr int cst_p_omega = 4;
 
 int main(int argc, char * argv[]) {
+    static_assert(cst_p_omega > 0, "p_omega cannot be null.");
+    static_assert(is_pow2(cst_q), "q must be a power of 2.");
+    static_assert(cst_N == 60, "cst_N must be 60.");
 
     po::options_description desc("Options");
     desc.add_options()(
@@ -150,7 +41,7 @@ int main(int argc, char * argv[]) {
         "snr,s", po::value<float>()->default_value(-10.f), "targeted signal-to-noise ratio")(
         "runs,n", po::value<int>()->default_value(10), "number of monte-carlo runs")(
         "p_delta,d", po::value<int>()->default_value(1), "number of score calculated every q chips (No effect with time-sliding)")(
-        "p_omega,w", po::value<int>()->default_value(1), "number of rotation hypotheses")(
+        "step-denominator", po::value<int>()->default_value(1), "step denominator, maximum error = pi / (2 * step)")(
         "pn-file", po::value<string>()->required(), "pn sequence mat file to use")(
         "ovmod-file", po::value<string>()->required(), "overmodulation sequence file to use")(
         "alist-file", po::value<string>()->required(), "alist file to use")(
@@ -181,16 +72,7 @@ int main(int argc, char * argv[]) {
 
     const int threads = vm["threads"].as<int>();
 
-    const int p_omega = vm["p_omega"].as<int>();
-    if (std::find(p_omega_accepted, p_omega_accepted + p_omega_count, p_omega) == (p_omega_accepted + p_omega_count)) {
-        std::cerr << "\033[31m[Error]\033[0m p_omega value " << p_omega << " is not currently supported.\n"
-                  << "You can try amending " << __FILE__ << " at line " << defines_line + 1 << "." << std::endl;
-        return 1;
-    }
-
     // const int p_delta = vm["p_delta"].as<int>();
-
-    constexpr float rotation_span = two_pi_f;
 
     const string alist_path = vm["alist-file"].as<string>();
 
@@ -212,15 +94,17 @@ int main(int argc, char * argv[]) {
 
     fclose(alist_file);
 
-    if (std::find(q_accepted, q_accepted + q_count, q) == (q_accepted + q_count)) {
-        std::cerr << "\033[31m[Error]\033[0m q value " << q << " is not currently supported.\n"
-                  << "Amend " << __FILE__ << " at line " << defines_line + 3 << "." << std::endl;
+    if (q != cst_q) {
+        std::cerr << "\033[31m[Error]\033[0m q value (" << q
+                  << ") is not currently the one the program have been compiled for (" << cst_q << ").\n"
+                  << "You may have to reconfigure and rebuild the project to fix that." << std::endl;
         return 1;
     }
 
-    if (std::find(N_accepted, N_accepted + N_count, N) == (N_accepted + N_count)) {
-        std::cerr << "\033[31m[Error]\033[0m N value " << N << " is not currently supported.\n"
-                  << "Amend " << __FILE__ << " at line " << defines_line + 5 << "." << std::endl;
+    if (N != cst_N) {
+        std::cerr << "\033[31m[Error]\033[0m N value (" << N
+                  << ") is not currently the one the program have been compiled for (" << cst_N << ").\n"
+                  << "You may have to reconfigure and rebuild the project to fix that." << std::endl;
         return 1;
     }
 
@@ -232,7 +116,7 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    const string pn_name = "PN" + std::to_string(q);
+    const string pn_name = "PN" + std::to_string(cst_q);
     matvar_t *   pn_var  = Mat_VarRead(pn_file, pn_name.c_str());
     if (!bool(pn_var)) {
         cerr << "\033[31m[Error]\033[0m Can't find pn mat variable " << pn_name << " in " << pn_path << "\n  "
@@ -252,7 +136,7 @@ int main(int argc, char * argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    const vector<float> pn(pn_data, pn_data + q);
+    const vector<float> pn(pn_data, pn_data + cst_q);
 
     Mat_VarFree(pn_var);
     Mat_Close(pn_file);
@@ -290,18 +174,21 @@ int main(int argc, char * argv[]) {
     Mat_VarFree(ovmod_var);
     Mat_Close(pn_file);
 
+    const int step_denominator = vm["step-denominator"].as<int>();
+
+    const float rotation_span = (cst_p_omega - 1) * two_pi_f / step_denominator;
+
     vector<CGenerator> generators;
     for (int t = 0; t < threads; t++) {
         generators.emplace_back(alist_path, pn, ovmod, snr, rotation_span);
     }
 
-    void * detectors;
-
-    allocate_detector_vector(detectors, 0, 0, 0);
-
-    
-
-    delete_detector_vector(detectors, 0, 0, 0);
+    vector<CDetectorSerial<cst_N, cst_q, cst_p_omega, float>> detectors;
+    for (int t = 0; t < threads; t++) {
+        detectors.emplace_back(pn.data(), 450, step_denominator);
+    }
+    cout << "Symbol rotation: " << (detectors[0].symbol_rotation() / pi_f) << " pi" << endl;
+    cout << "Rotation span:    " << (rotation_span / pi_f) << " pi" << endl;
 
     return EXIT_SUCCESS;
 }
